ccef972be2de2fb374edb1130903fab66f3610de
Jérémie Villalon <villalon.jeremie@gmail.com>
2020-03-08 12:27:20 +0100

    initalisation module

---
+++ ImportCustom/Controller/Index/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace Expertime\ImportCustom\Controller\Index;
+
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo 'Execute Action Index_Index OK';
+        die();
+    }
+}
